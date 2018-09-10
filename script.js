525cd32c9c646ae6ef7092e66afbc9fbd72f1904
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-10 23:07:07 +0000

    Lesson 1 Node.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Workspace for Node.js 
+//What is Node.js: A platfrom that allows us to run JavaScript on a sever platform
+//Easily communicates with databases 
+//Allows us to read, delete, and update files 
+//uses JS
+//fast (non-blocking code)
+//Many open source packages
+//Good for real-time services like chatrooms 
+

51477c4a951d087d57bd20dc17dea052bf2ffeb5
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-11 21:56:42 +0000

    Lesson 3 Javascript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Lesson 3 of Node.js 
+//All about the v8 engine
+//A JS enginge takes Javascript and changes the code into machine code-something that the computer can understand. 
+//Machine code order: Javascript, C++, Assembly language, Machine Code 
+//Node.js is written in C++
+//V8 is imbedded into node.js 
+//When you pass the JS, it's changed into machine code
+//Node.js with V8: JavaScript-->C++, Node.js, V8-->Machine Code 

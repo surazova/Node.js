2f182e603cf1e1f7dec1df94efbf461bac5cc4b0
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-11 22:20:27 +0000

    Lesson 4 JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-//Lesson 4 of Node.js 
-//The global object is the window object in the browser 
-console.log('hello');
-//types node script to run the application 
-setTimeout(function(){
-    console.log('3 seconds have passed');
-    
-}, 3000);
-//Save, run, and then type "node script"
+//Lesson 5 Node.js
+//Modules
 
-var time = 0;
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time + '2 seconds have passed');
-    if (time > 5){
-        clearInterval(timer); //This sets when the time is going to clear 
-    }
-    
-}, 2000);
-//Save, run, and then type "node script"
-//"click COMMAND + C" to stop the application from running 
+//normal function statement 
 
-console.log(__dirname); //tells you which directory you are in 
-console.log(__filename); //tells you the filename that you are in 
+function sayHi(){
+    console.log('Hi');
+}
+
+sayHi(); //invokes the function 
+
+//Different:
+
+function callFunction(fun) {
+    fun();
+}
+
+//function expression 
+var sayBye = function() {
+    console.log('bye');
+};
+
+callFunction(sayBye);

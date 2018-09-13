5b0a3ffa1baa33c568f08f18b26f17b0ee3d1962
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-13 22:25:46 +0000

    Lesson 12 Node.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-//Lesson 11 Node.js 
-//Clients and Servers 
-//A client sends a request to a server and the server sends a response to the client 
+//Lesson 12 Node.js
+//Creating a server 
 
-//You use protocols to communicate 
-//Protocols are a set of rules that two sides agree with in oder to communicate 
-//FTP (file transfer protocol)
-//HTTP (Used for websites)
-//When the structure is decided, the information is sent 
-//Then TCP is used, which splits the data into small sections called packets 
-//Node.js allows us to open the connection between computers 
-//We tell node.js what information we want to respond to
+var http = require('http');
 
-//Ports 
-//If node.js is litening to requests in a particular port, what comes there will be read by node 
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url)  //saves the url of the request 
+    res.writeHead(200, {'Content_Type': 'text/plain'})  //so the browswer knows what to do with its content
+    res.end('Hey ninjas');
+}); 
 
+
+server.listen(3000, '127.0.0.1'); //the local server/ip address
+console.log('yo dawgs, now listening to port 3000');

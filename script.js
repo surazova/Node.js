b621154d9f6386a0b89b1c2fc2f8df308e714748
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-13 22:37:46 +0000

    Lesson 13 Node.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-//Lesson 12 Node.js
-//Creating a server 
-
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url)  //saves the url of the request 
-    res.writeHead(200, {'Content_Type': 'text/plain'})  //so the browswer knows what to do with its content
-    res.end('Hey ninjas');
-}); 
-
-
-server.listen(3000, '127.0.0.1'); //the local server/ip address
-console.log('yo dawgs, now listening to port 3000');
+//Lesson 13 Node.js 
+//Creating streams 
+//Buffer: a temporary storage spot for data that is being transferred 
+//the buffer is filled with data 
+//Stream: data that flows in time into the buffer, that collects small chunks of data, then it gets sent to the clients 
+//Streams in node.js can increase performance and transfer data

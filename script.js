e46ba8430520a3ce4cd6ad8f13d3eb0e7a5d3c2f
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-13 01:30:42 +0000

    Lesson 8 Javascript

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-//Lesson 7 node.js 
-//Modeule patterns 
-//There are three module patterns that you can use to export modules and run them 
+//Lesson 8 node.js 
+//The Event module 
+
+var events = require('events');  //require a core module, you only need the name of it. Whatever is returned, will be stored in events
+
+//you can create a custom emmiter 
+
+var myEmitter = new events.EventEmitter(); //EventEmitter is capital!
+
+myEmitter.on('someEvent', function(mssg){
+    console.log(mssg);
+});
+
+myEmitter.emit('someEvent', 'the event was emitted');
 
 
-var stuff = require('./stuff');  //changed the name of the file to "stuff"
+//second example:
 
-console.log(stuff.counter(['shawn', 'crystal', 'ryu']));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5)); //you need to access pi, so you write stuff.pi
+var events = require('events');
+
+var util = require('util');
+
+
+var Person = function(name) {
+    this.name = name; 
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person ('james');
+var mary = new Person('mary');
+var bob = new Person('Bob');
+var people = [james, mary, bob];
+
+people.forEach(function(person){
+   person.on('speak', function(mssg){
+       console.log(person.name + ' said: ' + mssg);
+   }); 
+});
+
+james.emit('speak', 'hey dudes');
+mary.emit('speak', 'wassap');

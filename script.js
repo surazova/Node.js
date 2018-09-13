5b88cf83f3a25d9d09a9950425a16e5e3cc04da8
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-13 23:11:49 +0000

    Lesson 15 Node.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-//Lesson 14 Node.js 
-//Streams:
-//Writable streams: allow node js to write data to a strem 
-//readable streams: allow node js to read data from a stream 
-//duplex: can read and write to a stream
+//Lesson 15 node.js
+//Writable streams 
 
 var http = require('http');
 var fs = require('fs');
 
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received:');
-    console.log(chunk);
+    myWriteStream.write(chunk);
+    
 });

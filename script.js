c70aa2e97b23ce7c650ab4b8494010a223cd8b3d
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-13 23:25:52 +0000

    :esson 16 node.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-//Lesson 15 node.js
-//Writable streams 
+//Lesson 16 node.js
+//Pipes Order: Read Stream, buffer, read stream, write stream 
 
 var http = require('http');
 var fs = require('fs');
 
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received:');
-    myWriteStream.write(chunk);
+var server = http.createServer(function(req, res) {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     
+    
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+myReadStream.pipe(myWriteStream);
+
 });
+
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000');

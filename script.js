f155ab9c785de5c4fa7bfc33433cfb93fdcf16d8
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-16 20:23:43 +0000

    Lesson 29 node.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//Lesson 28 node.js 
-//Serving Static Files (%Middleware)
-//Middleware_ the code that uns between the request and the response 
+//Lesson 29 node.js 
+//Query Strings 
+//Additional data added to HTTP requests 
 
 var express = require('express');
 
@@ -9,16 +9,16 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets')); //exoress knows to use the file that is inside assets 
 
-
-
 app.get('/', function(req,res){
    res.render('index'); //change send file to render because you already created the partial views
 });
+
 app.get('/contact', function(req,res){
-   res.render('contact');
+  //type: https://github-surazova.c9users.io/contact?dept=marketing&person=joe (into the browser)
+   res.render('contact', {qs: req.query});
 });
 
-app.get('/profile/:name', function(req,res){  //you can access this parameter id is a number 
+app.get('/profile/:name', function(req,res){  
 var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
 res.render('profile', {person: req.params.name, data: data});
 });
@@ -27,13 +27,6 @@ res.render('profile', {person: req.params.name, data: data});
 app.listen(process.env.PORT, process.env.IP, 3000);
 
 
-
-
-//HTTP Methods: types of requests that you can make. They each have specific info attached to them 
-//GET -app.get('route', fn)
-//POST -app.post('route', fn)
-//DELETE -app.delete('route', fn)
-//PUT
 
 
 //the site: https://github-surazova.c9users.io/

ecc2839bbbe5faa9023a8075f1e2e58ad2b9cb48
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-17 03:56:14 +0000

    Lesson 33 Node.js

---
+++ app.js
@@ -0,0 +1,26 @@
+//Lesson 33 node.js
+//Making a todo=app part 3
+
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files 
+app.use(express.static('./todo-list'));
+
+
+//fire controllers 
+todoController();
+
+//listen to port 
+app.listen(process.env.PORT, process.env.IP, 3000);
+console.log('You are listening to port 3000');
+
+// MVC structure:
+//Model: data (todo, users)
+    //Controller: Controls the app (sections, todoController, userController)
+    //View: Template files (todo.ejs, account, ejs)
